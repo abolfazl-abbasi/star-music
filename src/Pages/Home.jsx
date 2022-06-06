117e0c210bd255ca77a9a150f3274b49a9a97013
abolfazl <abol.costom.1@gmail.com>
2022-06-06 21:39:27 +0430

    new update

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -476,10 +476,10 @@ const HomePage = () => {
         id="menuBackground"
       >
         <div
-          className="col-span-7 sm:!col-span-4 md:!col-span-3 h-full bg-stone-900 p-2 px-4 relative right-full transition-all"
+          className="col-span-7 sm:!col-span-4 md:!col-span-3 h-full bg-stone-900 p-2 relative right-full transition-all"
           id="menu"
         >
-          <div className="mb-3 ml-3 relative">
+          <div className="mb-3 relative">
             <div
               className="w-28 h-8 rounded-full overflow-hidden bg-stone-600 p-1 flex items-center space-x-3 hover:bg-stone-500 accountsBtn"
               onClick={() => handleShowAccounts("open", 2)}
@@ -541,23 +541,23 @@ const HomePage = () => {
             ></div>
           </div>
           <div>
-            <div className="flex items-center px-1 py-2 rounded-2xl my-1 mx-2 cursor-pointer text-stone-400 hover:bg-stone-700 hover:text-stone-100">
+            <div className="flex items-center px-1 py-2 rounded-2xl my-1 cursor-pointer text-stone-400 hover:bg-stone-700 hover:text-stone-100">
               <HiHome className="w-5 h-5 ml-2 mr-2" />
               <div className="text">Home</div>
             </div>
-            <div className="flex items-center px-1 py-2 rounded-2xl my-1 mx-2 cursor-pointer text-stone-400 hover:bg-stone-700 hover:text-stone-100">
+            <div className="flex items-center px-1 py-2 rounded-2xl my-1 cursor-pointer text-stone-400 hover:bg-stone-700 hover:text-stone-100">
               <HiSearch className="w-5 h-5 ml-2 mr-2" />
               <div className="text">Search</div>
             </div>
-            <div className="flex items-center px-1 py-2 rounded-2xl my-1 mx-2 cursor-pointer text-stone-400 hover:bg-stone-700 hover:text-stone-100">
+            <div className="flex items-center px-1 py-2 rounded-2xl my-1 cursor-pointer text-stone-400 hover:bg-stone-700 hover:text-stone-100">
               <HiLibrary className="w-5 h-5 ml-2 mr-2" />
               <div className="text">Library</div>
             </div>
-            <div className="flex items-center px-1 py-2 rounded-2xl my-1 mx-2 cursor-pointer text-stone-400 hover:bg-stone-700 hover:text-stone-100">
+            <div className="flex items-center px-1 py-2 rounded-2xl my-1 cursor-pointer text-stone-400 hover:bg-stone-700 hover:text-stone-100">
               <HiClock className="w-5 h-5 ml-2 mr-2" />
               <div className="text">Last Played</div>
             </div>
-            <div className="flex items-center px-1 py-2 rounded-2xl my-1 mx-2 cursor-pointer text-stone-400 hover:bg-stone-700 hover:text-stone-100">
+            <div className="flex items-center px-1 py-2 rounded-2xl my-1 cursor-pointer text-stone-400 hover:bg-stone-700 hover:text-stone-100">
               <HiCog className="w-5 h-5 ml-2 mr-2" />
               <div className="text">Setting</div>
             </div>
@@ -565,24 +565,24 @@ const HomePage = () => {
           <hr className="text-stone-300 m-4" />
           <div>
             <div className="">
-              <div className="px-4">
+              <div className="px-2">
                 <div className="text-xl text-stone-400">Playlist</div>
               </div>
             </div>
             <div>
-              <div className="flex items-center py-2 px-1 rounded-2xl my-1 mx-2 cursor-pointer mt-1 text-stone-400 hover:bg-stone-700 hover:text-stone-100">
+              <div className="flex items-center py-2 px-1 rounded-2xl my-1 cursor-pointer mt-1 text-stone-400 hover:bg-stone-700 hover:text-stone-100">
                 <div className="w-8 h-8 rounded-xl bg-purple-800 text-white ml-2 mr-3 flex items-center justify-center">
                   <HiStar className="w-5 h-5" />
                 </div>
                 <div>your marks</div>
               </div>
-              <div className="flex items-center py-2 px-1 rounded-2xl my-1 mx-2 cursor-pointer text-stone-400 hover:bg-stone-700 hover:text-stone-100">
+              <div className="flex items-center py-2 px-1 rounded-2xl my-1 cursor-pointer text-stone-400 hover:bg-stone-700 hover:text-stone-100">
                 <div className="w-8 h-8 rounded-xl bg-red-800 text-white ml-2 mr-3 flex items-center justify-center">
                   <HiHeart className="w-5 h-5" />
                 </div>
                 <div>your liking</div>
               </div>
-              <div className="flex items-center py-2 px-1 rounded-2xl my-1 mx-2 cursor-pointer text-stone-400 hover:bg-stone-700 hover:text-stone-100">
+              <div className="flex items-center py-2 px-1 rounded-2xl my-1 cursor-pointer text-stone-400 hover:bg-stone-700 hover:text-stone-100">
                 <div className="w-8 h-8 rounded-xl bg-cyan-800 text-white ml-2 mr-3 flex items-center justify-center">
                   <HiPlus className="w-5 h-5" />
                 </div>
